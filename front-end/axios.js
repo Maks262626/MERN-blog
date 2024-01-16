8ba36f29e5ecf2af6b7e4642019bff6d811b1c7a
Maks262626 <maxum2003la@gmail.com>
2024-01-16 20:53:11 +0200

    changing axios baseURL

--- front-end/axios.js
+++ front-end/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: "https://mern-blog-frsf.onrender.com",
 });
 
 instance.interceptors.request.use((config) => {
